4628f2bb45aeb4febd16ba1ac2ec679c32e63365
unknown <kresna.jenie@gmail.com>
2019-06-14 15:29:23 +0700

    Updated Wand and Flask

--- Wand/app_flask.py
+++ Wand/app_flask.py
@@ -6,23 +6,30 @@ from flask_restful import Resource, Api
 from flask_cors import CORS, cross_origin
 from flask import send_file
 
-import app_wand as wa
+import app_wand as drawing
 
 application = app = Flask(__name__)
 api = Api(app)
 
 class HelloWorld(Resource):
     def get(self):
-        return {'about': 'Hello World!'}
+        return "Hello World"
 
 api.add_resource(HelloWorld, '/')
 
-class GetVoucher(Resource):
-    def get(self, body):
-        filename = './score-output.png'
+class Name(Resource):
+    def get(self, nama, umur):
+        return "Hello " + nama+umur
+
+api.add_resource(Name, '/nama/<nama>/<umur>')
+
+class getLogoChelsea(Resource):
+    def get(self, skor):
+        filename = drawing.Draw(skor)
         return send_file(filename, mimetype='image/png')
 
-api.add_resource(GetVoucher, '/draw/score/<body>')
+api.add_resource(getLogoChelsea, '/getlogo/<skor>')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
